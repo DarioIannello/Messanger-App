00ffda0c8dec69901d4e0ba1e2066c81eca779aa
DarioIannello <dario.iannello1@gmail.com>
2022-12-15 18:16:38 +0100

    Initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:messangerapp/Authenticator.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChatApp());
 }
-
 
 class ChatApp extends StatelessWidget {
   @override
